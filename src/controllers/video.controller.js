0ff78c568a6c22848bc0a1a87c0075aac920cf40
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-18 23:24:36 +0530

    added delte video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -3,7 +3,20 @@ import ApiError from "../utils/ApiErrors.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { Video } from "../models/video.model.js";
+import deleteFromCloudinary from "../utils/delteFromCloudinary.js";
+//to get all videos for home page
+const getVideos = asyncHandler(async (req, res) => {
+  const videos = await Video.find({ isPublished: true })
+    .sort({ createdAt: -1 })
+    .populate("owner", "fullname username avtar");
 
+  console.log("videos =", videos);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "videos fetched successfully"));
+});
+
+//secured routes
 //to upload videos
 const uploadVideos = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
@@ -60,18 +73,6 @@ const uploadVideos = asyncHandler(async (req, res) => {
     );
 });
 
-//to get all videos for home page
-const getVideos = asyncHandler(async (req, res) => {
-  const videos = await Video.find({ isPublished: true })
-    .sort({ createdAt: -1 })
-    .populate("owner", "fullname username avtar");
-
-  console.log("videos =", videos);
-  return res
-    .status(200)
-    .json(new ApiResponse(200, videos, "videos fetched successfully"));
-});
-
 //get user uploaded videos in user profile
 const getUserVideos = asyncHandler(async (req, res) => {
   //find vedios of user
@@ -87,4 +88,39 @@ const getUserVideos = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, videos, "User videos fetched successfully"));
 });
 
-export { getUserVideos, uploadVideos, getVideos };
+//delete uploaded videos
+const deleteVideo = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  //if you are the owner or not
+  if (video.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not allowed to delete this video");
+  }
+
+  //delete from cloudinary before deleting from db
+  await deleteFromCloudinary(video.videoFile);
+  await deleteFromCloudinary(video.thumbnail);
+
+  const deletedVideo = await Video.findOneAndDelete({
+    _id: videoId,
+  });
+
+  console.log("deleted video=", deletedVideo);
+
+  if (!deletedVideo) {
+    throw new ApiError(404, "Video not found or not authorized");
+  }
+
+  //if found
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Video deleted successfully"));
+});
+
+export { getUserVideos, uploadVideos, getVideos, deleteVideo };
