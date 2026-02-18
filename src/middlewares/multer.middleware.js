5cda4e39238602eb67372616f5ad6195708c960f
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-18 12:23:57 +0530

    changed limit in multer middleware

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -10,4 +10,7 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage });
+export const upload = multer({
+  storage,
+  limits: { fileSize: 100 * 1024 * 1024 }, // 100MB limit
+});
