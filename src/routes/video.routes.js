888a6da87bc8ffecbd841a29952b1ba0f1e3d0ec
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-18 23:24:53 +0530

    added get user videos route and delete video functionality to video routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getVideos, uploadVideos } from "../controllers/video.controller.js";
-import router from "./user.routes.js";
+import {
+  getVideos,
+  uploadVideos,
+  getUserVideos,
+} from "../controllers/video.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
+//get all videos
 router.route("/get-videos").get(getVideos);
 
+//secured routes
 //upload video
 router.route("/upload-video").post(
   verifyJWT,
@@ -21,5 +27,10 @@ router.route("/upload-video").post(
   ]),
   uploadVideos
 );
+
+//get user videos
+router.route("/get-user-videos").get(verifyJWT, getUserVideos);
+
+router.route("/delete-video/:videoId").get(verifyJWT, deleteVideo);
 
 export default router;
