8c4edbd3df0d838e2cf434f46a9966bbbfacee2d
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-18 23:59:31 +0530

    refactor: improve code formatting and consistency in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiErrors.js";
 import jwt from "jsonwebtoken";
-import {User} from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   //access token from cookies
@@ -12,15 +12,18 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       req.header("Authorization")?.replace("Bearer ", "");
 
     //if token is not there throw an error
+
+    console.log("token is=", token);
+
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
-    console.log("token is =",token)
+    console.log("token is =", token);
     //if token is present
     //verify compare accesstoken secret present in cookie and in our server
     //and returns info that we've set at the time of token creation
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    console.log("decoded token=",decodedToken)
+    console.log("decoded token=", decodedToken);
 
     //now we have the data (_id) so we can get the instance
     const user = await User.findById(decodedToken?._id).select(
