71392d64652ea766b84f73f272e46995765cba18
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-20 16:19:57 +0530

    add subscription routes to app configuration

--- src/app.js
+++ src/app.js
@@ -32,13 +32,14 @@ app.use(express.static("public"));
 
 //routes section
 //routes import
-import userRouter from "../src/routes/user.routes.js";
-
-import videoRouter from "../src/routes/video.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter); //users is preffix
 app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
 //http://localhost:5000/api/v1/users/register
 
 export default app;
