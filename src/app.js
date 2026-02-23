6d8a9fcd4049e9804aba2ea66a784ce694e8b77a
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-23 16:11:49 +0530

    added and configured helmet pacakge

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 const app = express();
+app.use(helmet());
 
 app.use(
   cors({
